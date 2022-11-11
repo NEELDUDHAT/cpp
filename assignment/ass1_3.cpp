df546133a5a57ffcd315202b092ae675ec713904
Neel Dudhat <114645482+NEELDUDHAT@users.noreply.github.com>
2022-11-11 09:07:37 +0530

    Add files via upload

---
+++ assignment/ass1_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int boys, total = 45, girls;
+    boys = 17;
+    total = (80 * 45) / 100;
+    girls = total - boys;
+    cout << "number of girls who gain grade A=" << girls << endl;
+
+    return 0;
+}
